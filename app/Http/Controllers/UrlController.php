9b064d87232e47a1aac57cbb03dab39ea95256d1
Manish Ram <rammanish630@gmail.com>
2023-09-09 16:53:13 +0530

    add api endpoint

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -38,4 +38,31 @@ class UrlController extends Controller
         // Redirect to the original URL
         return Redirect::to($url->destination);
     }
+    public function shortenUrlApi(Request $request)
+    {
+        // Validate the request data
+        $this->validate($request, [
+            'destination' => 'required|url',
+        ]);
+
+        // Create a new shortened URL
+        $slug = Str::random(5);
+        $shortenedUrl = new Url([
+            'destination' => $request->input('destination'),
+            'slug' => $slug,
+        ]);
+        $shortenedUrl->save();
+
+        // Build the response JSON
+        $response = [
+            'destination' => $shortenedUrl->destination,
+            'slug' => $shortenedUrl->slug,
+            'updated_at' => $shortenedUrl->updated_at,
+            'created_at' => $shortenedUrl->created_at,
+            'id' => $shortenedUrl->id,
+            'shortened_url' => route('url.redirect', ['slug' => $shortenedUrl->slug]),
+        ];
+
+        return response()->json($response, 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UrlController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/v1/shorten-url', [UrlController::class, 'shortenUrlApi'])->name('shorten-url');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
