0886a799a12e22faccf13ee63ca680c2c1adb680
Manish Ram <rammanish630@gmail.com>
2023-09-09 23:26:01 +0530

    setup schedular

--- app/Models/Url.php
+++ app/Models/Url.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,10 +10,16 @@ class Url extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'destination', 'slug', 'views',
+        'destination', 'slug', 'views'
     ];
     public function getUrlData()
     {
         return config('app.url') . '/' . $this->slug;
+    }
+    public static function deleteStaleLinks()
+    {
+        $thirtyDaysAgo = Carbon::now()->subDays(30);
+
+        self::where('updated_at', '<', $thirtyDaysAgo)->delete();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
 Route::post('/v1/shorten-url', [UrlController::class, 'shortenUrlApi'])->name('shorten-url');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {return $request->user();});
 });
